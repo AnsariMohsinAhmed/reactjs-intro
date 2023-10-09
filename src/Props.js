48800ccc4b5090ad32d7bb59f8a33fabfafc6d44
AnsariMohsinAhmed <mohsinahmedansari99@gmail.com>
2023-10-09 23:15:31 +0530

    Updated this file

--- src/Props.js
+++ src/Props.js
@@ -1,3 +1,8 @@
 /*This file is continuation of States concept of States.js file
-ff
+
+    We will basically see the concept of "props" in this file, by sharing data between components
 */
+
+import { useState } from "react";
+
+export default Props(){}
