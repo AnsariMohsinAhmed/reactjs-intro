81c403846880d88fa9980cd7c12643790f5a9146
AnsariMohsinAhmed <mohsinahmedansari99@gmail.com>
2023-10-09 23:29:06 +0530

    Completed Props.js file

--- src/Props.js
+++ src/Props.js
@@ -3,6 +3,32 @@
     We will basically see the concept of "props" in this file, by sharing data between components
 */
 
-import { useState } from "react";
+import { useState } from 'react';
 
-export default Props(){}
+export default function Props() {
+
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+
+        <div>
+            <h1>Counters that update together</h1>
+            <MyButton count={count} onClick={handleClick} />
+            <MyButton count={count} onClick={handleClick} />
+        </div>
+    );
+}
+
+function MyButton({ count, onClick }) {
+
+    return (
+        
+        <button onClick={onClick}>
+            Clicked {count} times
+        </button>
+    );
+}
